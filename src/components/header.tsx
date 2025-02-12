2aaf6e83b758baee649af1708a5385bec784a54b
Michał Szymański <michal.szymanski92@gmail.com>
2025-02-12 21:44:49 +0100

    fix header layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,28 @@
 import ThemeToggle from '@/components/theme-toggle';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+import { House } from 'lucide-react';
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container flex h-14 items-center">
-                <div className="flex flex-1 items-center justify-between gap-2 md:justify-end">
-                    <ThemeToggle />
-                </div>
+            <div className="container mx-auto flex h-14 max-w-[1200px] items-center px-10">
+                <NavigationMenu className="flex w-full max-w-full flex-1 items-center justify-between gap-2">
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Link href="/" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    <House className="size-[1.2rem]" />
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <ThemeToggle />
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
             </div>
         </header>
     );
