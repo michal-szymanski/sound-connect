c2d923dd4b52722f8b56f4b6b939f940bf881794
Michał Szymański <michal.szymanski92@gmail.com>
2025-04-19 23:14:17 +0200

    cf

--- app.config.ts
+++ app.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         projects: ["./tsconfig.json"],
       }),
       tailwindcss(),
+      // cloudflarePlugin(),
     ],
     define: {
       "process.env": process.env,
