af623f72df095241cdd01a486d4d69cbc5e87e8a
Michał Szymański <michal.szymanski92@gmail.com>
2025-05-19 12:37:14 +0200

    fix ws

--- apps/web/src/routes/(main)/messages/index.tsx
+++ apps/web/src/routes/(main)/messages/index.tsx
@@ -3,10 +3,14 @@ import { getBindings } from '@/web/lib/cloudflare-bindings';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/(main)/messages/')({
-    component: RouteComponent
+    component: RouteComponent,
+    loader: async () => {
+        const { API_URL } = await getBindings();
+    }
 });
 
 function RouteComponent() {
-    const [websocket] = useWebsocket();
+    const { API_URL } = Route.useLoaderData();
+    const [websocket] = useWebsocket(API_URL);
     return <div>Hello "/messages/"!</div>;
 }
