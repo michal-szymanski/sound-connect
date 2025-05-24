dec55f84120ae2d35951bbcdd84cb8fff10a9ee8
Michał Szymański <michal.szymanski92@gmail.com>
2025-05-24 23:56:35 +0200

    guard api

--- apps/api/types.ts
+++ apps/api/types.ts
@@ -5,8 +5,8 @@ import constants from './constants';
 export type HonoContext = {
     Bindings: CloudflareBindings;
     Variables: {
-        user: typeof auth.$Infer.Session.user | null;
-        session: typeof auth.$Infer.Session.session | null;
+        user: typeof auth.$Infer.Session.user;
+        session: typeof auth.$Infer.Session.session;
     };
 };
 
