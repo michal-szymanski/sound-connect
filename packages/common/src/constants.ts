3b161aa90cf52b59f07b1548969d930bc2967cbc
Michał Szymański <michal.szymanski92@gmail.com>
2025-05-31 02:51:10 +0200

    Add post dialog

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,4 +1,5 @@
 export const DEFAULT_AVATAR_URL = 'https://github.com/shadcn.png';
 export const CHAT_MESSAGE_MAX_LENGTH = 1000;
+export const POST_TEXT_MAX_LENGTH = 1000;
 export const ONLINE_STATUS_INTERVAL = 2000;
 export const NOTIFICATION_RETENTION_TIME = 30 * 24 * 60 * 60;
