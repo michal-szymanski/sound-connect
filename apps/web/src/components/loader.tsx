f885acbca25515fb0952d27eb803c2e09ad1bb47
Michał Szymański <michal.szymanski92@gmail.com>
2025-06-01 18:24:22 +0200

    new loader

---
+++ apps/web/src/components/loader.tsx
@@ -0,0 +1,8 @@
+import { Ring } from 'ldrs/react';
+import 'ldrs/react/Ring.css';
+
+const Loader = () => {
+    return <Ring color="var(--muted-foreground)" size={15} stroke={2} />;
+};
+
+export default Loader;
