99bd17ec84f32f1fd4be88f8505f006bb29f15c7
Michał Szymański <michal.szymanski92@gmail.com>
2025-06-02 00:54:06 +0200

    clear query data on logout

--- apps/web/src/components/small/sign-out-button.tsx
+++ apps/web/src/components/small/sign-out-button.tsx
@@ -1,16 +1,19 @@
 import { DropdownMenuItem } from '@/web/components/ui/dropdown-menu';
 import { signOut } from '@/web/server-functions/auth';
+import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
 import { LogOut } from 'lucide-react';
 import { toast } from 'sonner';
 
 const SignOutButton = () => {
     const router = useRouter();
+    const queryClient = useQueryClient();
 
     const handleSignOut = async () => {
         const result = await signOut();
 
         if (result.success) {
+            queryClient.clear();
             router.navigate({ to: '/sign-in' });
         } else if (result.body) {
             toast.error('Could not sign out', {
