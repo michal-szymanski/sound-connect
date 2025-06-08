51692c5a4506e3524954dfade611a6aa2d47f406
Michał Szymański <michal.szymanski92@gmail.com>
2025-06-08 23:01:11 +0200

    cleanup

--- apps/web/src/components/not-found.tsx
+++ apps/web/src/components/not-found.tsx
@@ -1,6 +1,7 @@
 import { Link } from '@tanstack/react-router';
+import { ReactNode } from 'react';
 
-const NotFound = ({ children }: { children?: any }) => {
+const NotFound = ({ children }: { children?: ReactNode }) => {
     return (
         <div className="space-y-2 p-2">
             <div className="text-gray-600 dark:text-gray-400">{children || <p>The page you are looking for does not exist.</p>}</div>
