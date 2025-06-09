21fe58a166b30d83dafb4358f892bddf6ef8d417
Michał Szymański <michal.szymanski92@gmail.com>
2025-06-09 09:19:26 +0200

    improve styling for avatar button chat

--- apps/web/src/components/small/status-avatar.tsx
+++ apps/web/src/components/small/status-avatar.tsx
@@ -3,17 +3,19 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/web/components/ui/avatar'
 import { useWebSocket } from '@/web/providers/websocket-provider';
 import { DEFAULT_AVATAR_URL } from '@sound-connect/common/constants';
 import { UserDTO } from '@sound-connect/common/types/models';
+import { cn } from '@/web/lib/utils';
 
 type Props = {
     user: UserDTO;
+    className?: string;
 };
 
-const StatusAvatar = ({ user }: Props) => {
+const StatusAvatar = ({ user, className }: Props) => {
     const { statuses } = useWebSocket();
 
     return (
         <div className="relative">
-            <Avatar className="relative top-0">
+            <Avatar className={cn('relative top-0', className)}>
                 <AvatarImage src={user.image ?? DEFAULT_AVATAR_URL} />
                 <AvatarFallback>{user.name}</AvatarFallback>
             </Avatar>
