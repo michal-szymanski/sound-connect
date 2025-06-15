6a2f44b4ab5c9fd38931bd07e54a528af6b57f8a
Michał Szymański <michal.szymanski92@gmail.com>
2025-06-15 13:24:28 +0200

    refactor logger

--- apps/web/src/redux/middlewares/logger.ts
+++ apps/web/src/redux/middlewares/logger.ts
@@ -1,18 +1,35 @@
 import type { Action, Middleware } from '@reduxjs/toolkit';
 
+const colors = {
+    prevState: '#9E9E9E',
+    action: '#03A9F4',
+    nextState: '#4CAF50',
+    error: '#F20404'
+} as const;
+
 export const loggerMiddleware: Middleware = (store) => (next) => (action) => {
     if (process.env.NODE_ENV !== 'development') {
         return next(action);
     }
 
     const typedAction = action as Action;
-    console.group(`🔄 ${typedAction.type}`);
-    console.log('Previous State:', store.getState());
-    console.log('Action:', action);
+    const timestamp = new Date().toLocaleTimeString();
+
+    console.group(
+        `%caction %c${typedAction.type} %c@ ${timestamp}`,
+        `color: ${colors.action}; font-weight: bold;`,
+        `color: ${colors.nextState}; font-weight: bold;`,
+        `color: ${colors.prevState}; font-weight: normal;`
+    );
+
+    console.log('%cprev state', `color: ${colors.prevState}; font-weight: bold;`, store.getState());
+
+    console.log('%caction    ', `color: ${colors.action}; font-weight: bold;`, action);
 
     const result = next(action);
 
-    console.log('Next State:', store.getState());
+    console.log('%cnext state', `color: ${colors.nextState}; font-weight: bold;`, store.getState());
+
     console.groupEnd();
 
     return result;
