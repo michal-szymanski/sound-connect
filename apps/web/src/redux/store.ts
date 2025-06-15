4cb466ca4c3998307bf41cdda8ec672567350465
Michał Szymański <michal.szymanski92@gmail.com>
2025-06-15 13:13:44 +0200

    cleanup

--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -1,23 +1,6 @@
-import { configureStore, Middleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import uiReducer from './slices/ui-slice';
-
-const loggerMiddleware: Middleware = (store) => (next) => (action) => {
-    if (process.env.NODE_ENV !== 'development') {
-        return next(action);
-    }
-
-    const actionWithType = action as { type: string };
-    console.group(`🔄 ${actionWithType.type}`);
-    console.log('Previous State:', store.getState());
-    console.log('Action:', action);
-
-    const result = next(action);
-
-    console.log('Next State:', store.getState());
-    console.groupEnd();
-
-    return result;
-};
+import { loggerMiddleware } from '@/web/redux/middlewares/logger';
 
 export const store = configureStore({
     reducer: {
