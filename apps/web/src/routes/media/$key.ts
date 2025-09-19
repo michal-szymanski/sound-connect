d81a46042098c9128433f351aa6b8700a54c8176
Michał Szymański <michal.szymanski92@gmail.com>
2025-09-19 13:47:56 +0200

    fix proxy

--- apps/web/src/routes/media/$key.ts
+++ apps/web/src/routes/media/$key.ts
@@ -2,13 +2,14 @@ import { getBindings } from '@/web/lib/cloudflare-bindings';
 import { createServerFileRoute } from '@tanstack/react-start/server';
 
 export const ServerRoute = createServerFileRoute('/media/$key').methods({
-    GET: async ({ params }) => {
+    GET: async ({ params, request }) => {
         const { API_URL } = getBindings();
         const { key } = params;
 
         return new Response(null, {
             status: 302,
             headers: {
+                ...request.headers,
                 Location: `${API_URL}/media/${key}`
             }
         });
