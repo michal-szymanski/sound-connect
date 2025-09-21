fec14bc0d4de6e339328401f57d2486871aec15e
Michał Szymański <michal.szymanski92@gmail.com>
2025-09-21 11:21:06 +0200

    fix account button z index

--- apps/web/src/components/small/account-button.tsx
+++ apps/web/src/components/small/account-button.tsx
@@ -1,10 +1,10 @@
 import SignOutButton from '@/web/components/small/sign-out-button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/web/components/ui/avatar';
-import { Button } from '@/web/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/web/components/ui/dropdown-menu';
 import { useUser } from '@/web/lib/react-query';
 import { DEFAULT_AVATAR_URL } from '@sound-connect/common/constants';
 import { SidebarMenuButton } from '@/web/components/ui/sidebar';
+import clsx from 'clsx';
 
 type Props = {
     isCollapsed: boolean;
@@ -30,15 +30,15 @@ export const AccountButton = ({ isCollapsed }: Props) => {
                         <AvatarFallback className="bg-primary text-primary-foreground">{user.name?.[0]?.toUpperCase() || 'U'}</AvatarFallback>
                     </Avatar>
                     <span
-                        className={`truncate transition-all duration-300 ${
-                            isCollapsed ? 'w-0 overflow-hidden opacity-0' : 'ml-2 hidden w-auto opacity-100 xl:block'
-                        }`}
+                        className={clsx('ml-2 hidden w-auto truncate opacity-100 transition-all duration-300 xl:block', {
+                            'w-0 overflow-hidden opacity-0': isCollapsed
+                        })}
                     >
                         {user.name}
                     </span>
                 </DropdownMenuTrigger>
             </SidebarMenuButton>
-            <DropdownMenuContent className="z-52" align="start">
+            <DropdownMenuContent className="z-90" align="end">
                 <SignOutButton />
             </DropdownMenuContent>
         </DropdownMenu>
