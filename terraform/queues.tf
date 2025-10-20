3df1d1f5ffb6b9cf29f2b12d7d2b979232d9a82f
Michał Szymański <michal.szymanski92@gmail.com>
2025-10-20 23:22:51 +0200

    remove terraform

--- terraform/queues.tf
+++
@@ -1,9 +0,0 @@
-resource "cloudflare_queue" "posts_queue_dlq" {
-  account_id = var.cloudflare_account_id
-  name       = "posts-queue-dlq"
-}
-
-resource "cloudflare_queue" "posts_queue" {
-  account_id = var.cloudflare_account_id
-  name       = "posts-queue"
-}
--- terraform/r2-bucket.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_r2_bucket" "users_bucket" {
-  account_id = var.cloudflare_account_id
-  name       = "users-bucket"
-  location   = "auto"
-}
