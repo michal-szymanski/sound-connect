dc2a583934e9a10bf131d39613fde0c517ba93fc
Michał Szymański <michal.szymanski92@gmail.com>
2025-10-25 23:52:11 +0200

    add prettier to each project

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 160
+};
+
+export default config;
