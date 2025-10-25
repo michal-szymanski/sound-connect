8484d36f7c4a6c9950c932bc7cfcbb9fc68018b6
Michał Szymański <michal.szymanski92@gmail.com>
2025-10-25 23:52:11 +0200

    add prettier to each project

---
+++ apps/web/prettier.config.mjs
@@ -0,0 +1,7 @@
+import rootConfig from '../../prettier.config.mjs';
+
+/** @type {import("prettier").Config} */
+export default {
+    ...rootConfig,
+    plugins: ['prettier-plugin-tailwindcss']
+};
