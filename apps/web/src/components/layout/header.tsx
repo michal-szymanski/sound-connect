98dbc8c3bad281c4265fc2d4fc3662ea3b559a32
Michał Szymański <michal.szymanski92@gmail.com>
2025-10-28 19:49:52 +0100

    notifications button

--- apps/web/src/components/layout/header.tsx
+++ apps/web/src/components/layout/header.tsx
@@ -1,10 +1,12 @@
 import SearchBar from '@/web/components/layout/searchbar';
+import { NotificationsButton } from '@/web/components/notifications-button';
 
 const Header = () => {
     return (
         <header className="border-grid bg-background/95 supports-backdrop-filter:bg-background/60 fixed top-0 z-30 w-full overflow-visible border-b backdrop-blur-sm">
             <div className="flex h-14 items-center justify-center overflow-visible px-6">
                 <SearchBar />
+                <NotificationsButton />
             </div>
         </header>
     );
