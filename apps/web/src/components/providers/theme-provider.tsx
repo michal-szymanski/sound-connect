060e5503c5b009ed3bb37094ee92efc5a1caaada
Michał Szymański <michal.szymanski92@gmail.com>
2025-11-08 22:56:38 +0100

    Fix theme switcher

---
+++ apps/web/src/components/providers/theme-provider.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import type { ReactNode } from 'react';
+
+type Props = {
+    children: ReactNode;
+};
+
+export function ThemeProvider({ children }: Props) {
+    return (
+        <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            {children}
+        </NextThemesProvider>
+    );
+}
