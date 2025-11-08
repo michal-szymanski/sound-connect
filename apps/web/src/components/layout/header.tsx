1df94e635957a7bd84567aaca0ab58849a62465f
Michał Szymański <michal.szymanski92@gmail.com>
2025-11-08 22:54:03 +0100

    update theme

--- apps/web/src/components/layout/header.tsx
+++ apps/web/src/components/layout/header.tsx
@@ -1,12 +1,15 @@
 import SearchBar from '@/web/components/layout/searchbar';
-import { NotificationsButton } from '@/web/components/notifications-button';
+import ThemeToggle from '@/web/components/small/theme-toggle';
 
 const Header = () => {
     return (
         <header className="border-grid bg-background/95 supports-backdrop-filter:bg-background/60 fixed top-0 z-30 w-full overflow-visible border-b backdrop-blur-sm">
-            <div className="flex h-14 items-center justify-center overflow-visible px-6">
+            <div className="flex h-14 items-center overflow-visible px-6">
+                <div className="flex-1" />
                 <SearchBar />
-                <NotificationsButton />
+                <div className="flex flex-1 justify-end">
+                    <ThemeToggle />
+                </div>
             </div>
         </header>
     );
